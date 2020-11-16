18c9ea0c37da26df410113d8ebe669630595934c
Jakeob Ewell <jakeob_e@yahoo.com>
2020-11-16 14:08:21 -0700

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+
+var fullName = 'Jakeob Ewell';
+var isCool = true;
+var totalPets = 1;
+
+console.log('Full Name:', fullName);
+console.log('Jakeob is cool:', isCool);
+console.log('Jakeob has how many pets?:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
+
+var unknown = null;
+
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never)
