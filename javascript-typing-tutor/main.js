74acae9e64c50d1985645ecae8a1320707fcafa6
Jakeob Ewell <jakeob_e@yahoo.com>
2020-11-25 13:30:10 -0700

    almost finished

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,15 +1,15 @@
 var letters = document.querySelectorAll("span");
 
+var i = 0;
+
 addEventListener('keydown', function (event) {
 
-for (var i = 0; i < letters.length; i++) {
   if (event.key === letters[i].textContent) {
     letters[i].className = 'correct';
     letters[i + 1].className = 'current-letter';
+    i += 1;
   }
   else {
     letters[i].className = 'wrong';
-    break;
     }
-  }
 })
