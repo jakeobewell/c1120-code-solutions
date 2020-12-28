d29c9bbdfead6993b68c2e41e03efa12b9063a60
Jakeob Ewell <jakeob_e@yahoo.com>
2020-12-28 14:33:52 -0700

    in progress title case

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,4 +1,18 @@
 /* exported isAnagram */
-function isAnagram() {
+function isAnagram(firstString, secondString) {
+  var newStringOne = firstString.replaceAll(' ', '').toLowerCase();
+  var newStringTwo = secondString.replaceAll(' ', '').toLowerCase();
 
+  newStringOne = newStringOne.split('');
+  newStringTwo = newStringTwo.split('');
+
+  newStringOne = newStringOne.sort().join('');
+  newStringTwo = newStringTwo.sort().join('');
+
+  if (newStringOne === newStringTwo) {
+    return true
+  }
+  else {
+    return false;
+  }
 }
--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,19 @@
 /* exported reverseWords */
+function reverseWords(string) {
+
+var arr = string.split(' ');
+var newArr = [];
+
+for (var i = 0; i < arr.length; i++) {
+  var string = arr[i];
+  var newString = '';
+  for (x = string.length - 1; x >= 0; x--) {
+    newString += string[x];
+  }
+  newArr.push(newString);
+}
+
+var reverse = newArr.join(' ');
+
+return reverse;
+}
