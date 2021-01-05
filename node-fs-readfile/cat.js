a3c3dce381865d7da93573a3829ecff9445118a3
Jakeob Ewell <jakeob_e@yahoo.com>
2021-01-05 11:08:53 -0700

    still working

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 const textFiles = process.argv;
 
 const logFiles = () => {
-let files = '';
 
   for (let i = 2; i < textFiles.length; i++) {
+    let files = '';
     fs.readFile(textFiles[i], 'utf8', (err, data) => {
       if (err) throw err;
-      console.log(data);
+      files += (data + '\n');
+      }
     });
   }
 }
