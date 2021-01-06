979173eb9b0aeebb52b5635733e25bf5537b77a0
Jakeob Ewell <jakeob_e@yahoo.com>
2021-01-06 16:32:03 -0700

    completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+}
+
+let gradesArr = [];
+
+for (const object in grades) {
+  gradesArr.push(grades[object]);
+}
+
+app.get('/api/grades', function (req, res) {
+  res.json(gradesArr);
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000')
+})
