4f1b384fa0dd22e0b8926846e5ab7ce2209033d3
Jakeob Ewell <jakeob_e@yahoo.com>
2021-01-19 16:18:47 -0700

    completed react-hot-button-component

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -8,15 +8,65 @@ export default class HotButton extends React.Component {
     this.state = { clicked: 0 }
   }
   handleClick() {
-    this.setState({ clicked: (clicked + 1) })
+    this.setState({ clicked: this.state.clicked + 1 })
   }
 
   render() {
-    if (this.state.click < 3) {
-      return <button className="stage-one" onClick={this.handleClick}>Hot Button</button>
+    if (this.state.clicked < 3) {
+      return (
+      <div>
+        <button onClick={this.handleClick}>Hot Button</button>
+        <h3>Click Count {this.state.clicked}</h3>
+      </div>
+      );
     }
-    if (this.state.click < 6) {
-      return <button className="stage-two" onClick={this.handleClick}>Hot Button</button>
+    if (this.state.clicked < 6) {
+      return (
+      <div>
+        <button className="stage-one" onClick={this.handleClick}>Hot Button</button>
+        <h3>Click Count {this.state.clicked}</h3>
+      </div>
+      );
+    }
+    if (this.state.clicked < 9) {
+      return (
+        <div>
+          <button className="stage-two" onClick={this.handleClick}>Hot Button</button>
+          <h3>Click Count {this.state.clicked}</h3>
+        </div>
+      );
+    }
+    if (this.state.clicked < 12) {
+      return (
+        <div>
+          <button className="stage-three" onClick={this.handleClick}>Hot Button</button>
+          <h3>Click Count {this.state.clicked}</h3>
+        </div>
+      );
+    }
+    if (this.state.clicked < 15) {
+      return (
+        <div>
+          <button className="stage-four" onClick={this.handleClick}>Hot Button</button>
+          <h3>Click Count {this.state.clicked}</h3>
+        </div>
+      );
+    }
+    if (this.state.clicked < 18) {
+      return (
+        <div>
+          <button className="stage-five" onClick={this.handleClick}>Hot Button</button>
+          <h3>Click Count {this.state.clicked}</h3>
+        </div>
+      );
+    }
+    if (this.state.clicked >= 18) {
+      return (
+        <div>
+          <button className="stage-six" onClick={this.handleClick}>Hot Button</button>
+          <h3>Click Count {this.state.clicked}</h3>
+        </div>
+      );
     }
   }
 }
