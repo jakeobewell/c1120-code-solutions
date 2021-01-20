3f1e1089688832aa7541f1a1b9049bf22987e8e9
Jakeob Ewell <jakeob_e@yahoo.com>
2021-01-20 13:19:26 -0700

    completed react-stopwatch-component

---
+++ react-stopwatch-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Stopwatch from "./stopwatch";
+
+const watch = <Stopwatch />;
+
+ReactDOM.render(watch, document.getElementById('root'));
